180d27475b42c857ac2e3146c6677dfcb3d3dd6f
Frydesk <josepheudave@hotmail.com>
2023-04-17 22:41:41 -0600

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,31 +1,9 @@
-var container = document.getElementById('particle-container');
+/* ---- particles.js config ---- */
 
-container.addEventListener('click', function(event) {
-  // Emit particles at the click/touch position
-  particlesJS.emit('particles', event.clientX, event.clientY);
-});
-
-container.addEventListener('touchstart', function(event) {
-  // Emit particles at the click/touch position
-  particlesJS.emit('particles', event.touches[0].clientX, event.touches[0].clientY);
-});
-
-var container = document.getElementById('particle-container');
-
-container.addEventListener('mousemove', function(event) {
-  // Emit particles at the mouse position
-  particlesJS.emit('particles', event.clientX, event.clientY);
-});
-
-container.addEventListener('touchmove', function(event) {
-  // Emit particles at the touch position
-  particlesJS.emit('particles', event.touches[0].clientX, event.touches[0].clientY);
-});
-
-particlesJS('particle-container', {
+particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 50,
+      "value": 380,
       "density": {
         "enable": true,
         "value_area": 800
@@ -42,6 +20,11 @@ particlesJS('particle-container', {
       },
       "polygon": {
         "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
       }
     },
     "opacity": {
@@ -91,7 +74,7 @@ particlesJS('particle-container', {
     "events": {
       "onhover": {
         "enable": true,
-        "mode": "repulse"
+        "mode": "grab"
       },
       "onclick": {
         "enable": true,
@@ -101,7 +84,7 @@ particlesJS('particle-container', {
     },
     "modes": {
       "grab": {
-        "distance": 400,
+        "distance": 140,
         "line_linked": {
           "opacity": 1
         }
@@ -114,7 +97,8 @@ particlesJS('particle-container', {
         "speed": 3
       },
       "repulse": {
-        "distance": 200
+        "distance": 200,
+        "duration": 0.4
       },
       "push": {
         "particles_nb": 4
@@ -127,3 +111,23 @@ particlesJS('particle-container', {
   "retina_detect": true
 });
 
+
+/* ---- stats.js config ---- */
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
